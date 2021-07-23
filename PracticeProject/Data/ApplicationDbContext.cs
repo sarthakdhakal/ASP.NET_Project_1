41493b3718734dfd0e3017efb73dff725e463400
Sarthak <sarthakdhakal@gmail.com>
2021-07-23 06:54:57 +0545

    Initial commit

---
+++ PracticeProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticeProject.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
